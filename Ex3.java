66a78698f2d16932e924e9ec11a830edf73d739e
Guilherme <99288564+Guilherme4444@users.noreply.github.com>
2022-05-10 16:17:38 -0300

    Add files via upload

---
+++ Ex3.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+	
+        int number1;
+        int number2;
+        int number3;
+        
+        int soma;
+        int max;
+        int mim;
+        int numero3 = 0;
+        
+        System.out.println("Digite o primeiro número: ");
+        number1 = entrada.nextInt();
+        
+        System.out.println("Digite o segundo número: ");
+        number2 = entrada.nextInt();
+        
+        System.out.println("Digite o terceiro número: ");
+        number3 = entrada.nextInt();
+        
+         if (number1 > number2)
+              number1 = numero3;
+              number2 = numero3;
+            
+        
+        
+    }
+    }
+
+    
+    
+
+
+
+
+        
+        
+
+
+
+
+        }
+    }
+}
+        
+        
+
+
+        
